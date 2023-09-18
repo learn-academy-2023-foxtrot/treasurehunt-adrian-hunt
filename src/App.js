e8869d73446e7d318fe91408165c2c1bd66f64aa
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-09-18 12:02:35 -0700

    few updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
+import Square from "./components/Square"
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -13,10 +14,37 @@ const App = () => {
     "?",
     "?"
   ])
+  const [remainingGuesses, setRemainingGuesses] = useState(5);
+
+
+  const handleClick = (selectedIndex) => {
+    //return something
+
+    //treasure location
+    let treasureIndex = Math.floor(Math.random() * board.length);
+    
+    if(treasureIndex === selectedIndex) {
+
+      board[selectedIndex]= "☀️"
+      setBoard([...board])
+      alert("You win")
+    } else if (treasureIndex === selectedIndex){
+      board[selectedIndex]= "🏖️"
+      setBoard([...board])
+      alert("You lose")
+    } else {
+      
+    }
+  }
+
+ 
+
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <Square board={board} handleClick={handleClick}/>
+      <button>Reset Game</button>
     </>
   )
 }
